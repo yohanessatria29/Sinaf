9e1d76cde82fd34f7e1aaf6a8e38c101d73de2f4
Yohanessatria <yohanessatria29@gmail.com>
2025-05-05 14:48:53 +0700

    update database debug

--- application/models/Mlogin.php
+++ application/models/Mlogin.php
@@ -6,17 +6,12 @@ class Mlogin extends CI_Model
 
     function query_validasi_email($email)
     {
-        // $result = $this->db->query("SELECT db_akreditasi_non_rs.users.* FROM db_akreditasi_non_rs.users WHERE db_akreditasi_non_rs.users.email = '$email'  LIMIT 1");
-        // $result = $this->db->get_where('users', array('email' => $this->db->escape($email)), 1);
         $result = $this->db->get_where('users', array('email' => $email), 1);
         return $result;
-        // return $this->db->escape($email);
     }
 
     function query_validasi_password($email, $hashed)
     {
-        // $result = $this->db->query("SELECT db_akreditasi_non_rs.users.* FROM db_akreditasi_non_rs.users WHERE db_akreditasi_non_rs.users.email ='$email' AND db_akreditasi_non_rs.users.password_enkripsi ='$hashed'  LIMIT 1");
-        // $result = $this->db->get_where('users', array('email' => $this->db->escape($email), 'password_enkripsi' => $this->db->escape_like_str($hashed)), 1);
         $result = $this->db->get_where('users', array('email' => $email, 'password_enkripsi' => $hashed), 1);
         return $result;
     }
