515298d3488397947f2f68fd449e52b462c6a6b3
Yohanessatria <yohanessatria29@gmail.com>
2025-05-05 14:43:42 +0700

    update database

--- application/views/auth-login.php
+++ application/views/auth-login.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://sinaf.kemkes.go.id/ 'unsafe-inline'">
+    <!-- <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://sinaf.kemkes.go.id/ 'unsafe-inline'"> -->
     <title>Login - SINAF</title>
     <link rel="stylesheet" href="assets/css/main/app.css">
     <link rel="stylesheet" href="assets/css/pages/auth.css">
     <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
     <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
     <meta name="description" content="Halaman Sinaf">
-
 </head>
 
 <body>
