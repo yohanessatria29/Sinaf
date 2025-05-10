a7d89ba7a203d9f5035def2c4b07f652c136acae
Yohanessatria <yohanessatria29@gmail.com>
2025-05-10 11:00:08 +0700

    update hasil VA2

--- application/hooks/add_x_frame_options_header.php
+++ application/hooks/add_x_frame_options_header.php
@@ -1,8 +1,8 @@
+<?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-function add_x_frame_options_header() {
-// Get the CodeIgniter instance
-$CI =& get_instance();
-// Set the X-Frame-Options header
-$CI->output->set_header('X-Frame-Options: SAMEORIGIN');
+function add_x_frame_options_header()
+{
+    $CI = &get_instance();
+    $CI->output->set_header('X-Frame-Options: SAMEORIGIN');
 }
