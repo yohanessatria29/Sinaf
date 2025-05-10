0a797fd5cecb4966be7174eb645c65e3f1d75344
Yohanessatria <yohanessatria29@gmail.com>
2025-05-10 22:06:25 +0700

    update hasil VA 13

--- application/hooks/add_x_frame_options_header.php
+++ application/hooks/add_x_frame_options_header.php
@@ -11,19 +11,21 @@ function add_x_frame_options_header()
 // Function to set Content Security Policy Header
 function add_csp_header()
 {
-    $CI = &get_instance();
+    // $CI = &get_instance();
 
-    // Define the Content-Security-Policy header
-    $csp = "default-src 'self'; 
-            script-src 'self' https://apis.google.com https://cdn.datatables.net https://cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js https://code.jquery.com https://cdnjs.cloudflare.com; 
-            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; 
-            font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; 
-            img-src 'self' data:; 
-            connect-src 'self'; 
-            frame-src 'self';";
+    // // Define the Content-Security-Policy header
+    // $csp = "default-src 'self'; 
+    //         script-src 'self' https://apis.google.com https://cdn.datatables.net https://cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js https://code.jquery.com https://cdnjs.cloudflare.com; 
+    //         style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; 
+    //         font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; 
+    //         img-src 'self' data:; 
+    //         connect-src 'self'; 
+    //         frame-src 'self';";
 
-    // Set the header
-    $CI->output->set_header('Content-Security-Policy: ' . $csp);
+    // // Set the header
+    // $CI->output->set_header('Content-Security-Policy: ' . $csp);
+
+    header("Content-Security-Policy: default-src 'self'; script-src 'self' https://cdn.datatables.net https://code.jquery.com https://cdnjs.cloudflare.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data:; connect-src 'self'; frame-src 'self';");
 }
 
 
