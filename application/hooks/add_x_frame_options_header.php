9a70107fdd320d637137a4d0a44b0453a165680f
Yohanessatria <yohanessatria29@gmail.com>
2025-05-10 11:30:17 +0700

    update hasil VA 5

--- application/hooks/add_x_frame_options_header.php
+++ application/hooks/add_x_frame_options_header.php
@@ -12,5 +12,16 @@ function add_x_frame_options_header()
 function add_csp_header()
 {
     $CI = &get_instance();
-    $CI->output->set_header('Content-Security-Policy: default-src \'self\'; script-src \'self\' https://apis.google.com; style-src \'self\' https://fonts.googleapis.com; font-src \'self\' https://fonts.gstatic.com;');
+
+    // Define the Content-Security-Policy header
+    $csp = "default-src 'self'; 
+            script-src 'self' https://apis.google.com https://cdn.datatables.net https://cdn.datatables.net/fixedheader/3.1.5/js/dataTables.fixedHeader.min.js https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js https://code.jquery.com https://cdnjs.cloudflare.com; 
+            style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; 
+            font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; 
+            img-src 'self' data:; 
+            connect-src 'self'; 
+            frame-src 'self';";
+
+    // Set the header
+    $CI->output->set_header('Content-Security-Policy: ' . $csp);
 }
