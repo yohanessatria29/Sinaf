f33ca366d4952d27afa902380aa93e2ed74283e7
Yohanessatria <yohanessatria29@gmail.com>
2025-05-11 13:49:58 +0700

    update hasil VA 19

--- index.php
+++ index.php
@@ -54,8 +54,6 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 
 /*
