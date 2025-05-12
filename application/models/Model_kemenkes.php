7f8876f4993bab749f9e8d1920c9c642d4bee519
Yohanessatria <yohanessatria29@gmail.com>
2025-05-12 12:39:51 +0700

    update kemkes user

--- application/models/Model_kemenkes.php
+++ application/models/Model_kemenkes.php
@@ -225,8 +225,8 @@ class Model_kemenkes extends CI_Model
 		ORDER BY
 		a.created_at DESC");
 
-		echo $this->db->last_query(); // Untuk print query mentahnya
-		exit; // Supaya proses berhenti di sini (tidak lanjut ke controller)
+		// echo $this->db->last_query(); // Untuk print query mentahnya
+		// exit; // Supaya proses berhenti di sini (tidak lanjut ke controller)
 		return $sql->result_array();
 	}
 
